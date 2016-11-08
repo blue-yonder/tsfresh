bf9e830ed21fe146c706e4a95a4e422552a8def0
Julius Neuffer <jneuff@users.noreply.github.com>
2016-11-08 18:15:09 +0100

    Improve setup of default features (#48)
    * Restrict adding of None parameter to functions without parameters
    
    * Move setup of features and their parameters into __init__
    
    * Add unittest to ensure by default all features are calculated
    
    * Improve docstring in FeatureExtractionSettings

--- tests/feature_extraction/test_settings.py
+++ tests/feature_extraction/test_settings.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 import numpy as np
 from tsfresh.feature_extraction.settings import FeatureExtractionSettings
 import six
+from tsfresh.feature_extraction import feature_calculators
 
 class TestSettingsObject(TestCase):
 
@@ -45,3 +46,19 @@ class TestSettingsObject(TestCase):
 
         self.assertEqual(cset.kind_to_calculation_settings_mapping[tsn]["value_count"],
                          [{"value": np.PINF}, {"value": np.NINF}, {"value": np.NaN}])
+
+    def test_default_calculates_all_features(self):
+        """
+        Test that by default a FeatureExtractionSettings object should be set up to calculate all features defined
+        in tsfresh.feature_extraction.feature_calculators
+        :param self:
+        :return:
+        """
+        settings = FeatureExtractionSettings()
+        all_feature_calculators = [name for name, func in feature_calculators.__dict__.items()
+                                   if hasattr(func, "fctype")]
+
+        for calculator in all_feature_calculators:
+            self.assertIn(calculator, settings.name_to_param,
+                          msg='Default FeatureExtractionSettings object does not setup calculation of {}'
+                          .format(calculator))
