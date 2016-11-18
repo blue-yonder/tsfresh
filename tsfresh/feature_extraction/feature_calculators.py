blue-yonder/tsfresh

commit 3b40eb29540ccabb1d2446698edcf5b40bee24f3

fixed bug for fourier calculation when only coeff equal to 0 is passed

tsfresh/feature_extraction/feature_calculators.py
@@ -696,7 +696,7 @@ def fft_coefficient(x, c, param):
         if coeff < 0:
             raise ValueError("Coefficients must be positive or zero.")
 
-    maximum_coefficient = max(coefficients)
+    maximum_coefficient = min(max(coefficients), 0)
     fft = np.fft.rfft(x, min(len(x), 2 * maximum_coefficient))
 
     res = [fft[q] if q < len(fft) else 0 for q in coefficients]

commit 37a4ea8ba9abd501629dba689ef5f05e133a8801

Fixed bug that was introduced in 3b40eb29540ccabb1d2446698edcf5b40bee24f3

tsfresh/feature_extraction/feature_calculators.py
@@ -696,7 +696,7 @@ def fft_coefficient(x, c, param):
         if coeff < 0:
             raise ValueError("Coefficients must be positive or zero.")
 
-    maximum_coefficient = min(max(coefficients), 0)
+    maximum_coefficient = max(coefficients + [1])
     fft = np.fft.rfft(x, min(len(x), 2 * maximum_coefficient))
 
     res = [fft[q] if q < len(fft) else 0 for q in coefficients]