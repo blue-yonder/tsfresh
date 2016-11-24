0c25d7bccce8519647f0b8acd868cc43967eca20
mynameisvinn <vin.tang@gmail.com>
2016-11-24 13:28:16 -0500

    include test for augmented dickey fuller

--- tests/feature_extraction/test_feature_calculations.py
+++ tests/feature_extraction/test_feature_calculations.py
@@ -145,7 +145,7 @@ class FeatureCalculationTestCase(TestCase):
         self.assertAlmostEqualOnAllArrayTypes(mean_autocorrelation, x, expected_res)
 
     def test_augmented_dickey_fuller(self):
-        x = np.array([1,2,3,4,3,4,2,3])
+        x = [1,2,3,4,3,4,2,3]
         self.assertAlmostEqualOnAllArrayTypes(augmented_dickey_fuller, x, -2.6825663173365015) 
 
     def test_abs_energy(self):
