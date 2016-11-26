71fd7d8dfa0e32ace2170261a66731eaa148253f
mynameisvinn <vin.tang@gmail.com>
2016-11-26 08:53:14 -0500

    replace autolag w maxlag

--- tsfresh/feature_extraction/feature_calculators.py
+++ tsfresh/feature_extraction/feature_calculators.py
@@ -267,7 +267,11 @@ def augmented_dickey_fuller(x):
     """
 
     try:
+<<<<<<< HEAD
         return adfuller(x, autolag='AIC')[0] # 0th element refers to test statistic
+=======
+        return adfuller(x, 1)[0] # By default maxlag is set to None, while it should be set to integer.
+>>>>>>> parent of 652aa16... clean up comments
     except LinAlgError:
         return np.NaN
     except ValueError:  # occurs if sample size is too small
