24fd382c0de75ea86688ffc82c841282e3adb9d0
mynameisvinn <vin.tang@gmail.com>
2016-11-26 08:48:28 -0500

    use autolag for dickey fuller cal

--- tsfresh/feature_extraction/feature_calculators.py
+++ tsfresh/feature_extraction/feature_calculators.py
@@ -266,10 +266,8 @@ def augmented_dickey_fuller(x):
     :return type: float
     """
 
-    maxlag = 1 # by default maxlag is set to None, when it should be set to integer.
     try:
-
-        return adfuller(x, maxlag)[0] # 0th element refers to test statistic
+        return adfuller(x, autolag='AIC')[0] # 0th element refers to test statistic
     except LinAlgError:
         return np.NaN
     except ValueError:  # occurs if sample size is too small
