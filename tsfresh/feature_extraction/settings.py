86ef99206fa8cdb00b47511f76f10bb4cf8ddb0d
Maximilian Christ <max.christ@me.com>
2016-11-28 15:05:45 +0100

    env variable is returned as string not integer

--- tsfresh/feature_extraction/settings.py
+++ tsfresh/feature_extraction/settings.py
@@ -100,7 +100,7 @@ class FeatureExtractionSettings(object):
             })
 
         # default None means one procesqs per cpu
-        self.n_processes = int((os.getenv("NUMBER_OF_CPUS") or cpu_count()) /2) + 1
+        self.n_processes = int(int(os.getenv("NUMBER_OF_CPUS") or cpu_count())/2) + 1
 
     def set_default_parameters(self, kind):
         """
