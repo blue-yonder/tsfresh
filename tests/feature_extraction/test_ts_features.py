adf85b85347af7887cf9a1c7b432df35ad8630da
Maximilian Christ <max.christ@me.com>
2016-11-28 15:45:26 +0100

    added unit tests for _extract_features_for_one_time_series

--- tests/feature_extraction/test_ts_features.py
+++ tests/feature_extraction/test_ts_features.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division
 import numpy as np
 import pandas as pd
 from tests.fixtures import DataTestCase
-from tsfresh.feature_extraction.extraction import extract_features
+from tsfresh.feature_extraction.extraction import extract_features, _extract_features_for_one_time_series
 from tsfresh.feature_extraction.settings import FeatureExtractionSettings
 import six
 import os
@@ -41,6 +41,45 @@ class FeatureExtractorTestCase(DataTestCase):
         self.assertTrue(np.all(extracted_features_sts.a__maximum == np.array([1.0, 6.0])))
         self.assertTrue(np.all(extracted_features_sts.a__sum_values == np.array([1.0, 11.0])))
         self.assertTrue(np.all(extracted_features_sts.a__count_above_mean == np.array([0, 1])))
+
+    def test_calculate_ts_features_for_one_time_series(self):
+        # todo: implement more methods and test more aspects
+        df = self.create_test_data_sample()
+        extracted_features = _extract_features_for_one_time_series(["b", df.loc[df.kind == "b", ["val", "id"]]],
+                                                                   settings=self.settings,
+                                                                   column_value="val", column_id="id")
+
+        self.assertIsInstance(extracted_features, pd.DataFrame)
+        self.assertTrue(np.all(extracted_features.b__sum_values == np.array([757, 695])))
+        self.assertTrue(np.all(extracted_features.b__minimum == np.array([3, 1])))
+        self.assertTrue(np.all(extracted_features.b__abs_energy == np.array([36619, 35483])))
+        self.assertTrue(np.all(extracted_features.b__mean == np.array([37.85, 34.75])))
+        self.assertTrue(np.all(extracted_features.b__median == np.array([39.5, 28.0])))
+
+        df_sts = self.create_one_valued_time_series()
+        extracted_features_sts = _extract_features_for_one_time_series(["a", df_sts[["val", "id"]]],
+                                                                       settings=self.settings,
+                                                                       column_value="val", column_id="id")
+
+        self.assertIsInstance(extracted_features_sts, pd.DataFrame)
+        self.assertTrue(np.all(extracted_features_sts.a__maximum == np.array([1.0, 6.0])))
+        self.assertTrue(np.all(extracted_features_sts.a__sum_values == np.array([1.0, 11.0])))
+        self.assertTrue(np.all(extracted_features_sts.a__count_above_mean == np.array([0, 1])))
+
+    def test_calculate_ts_features_after_randomisation(self):
+        df = self.create_test_data_sample()
+        df_random = df.copy().sample(frac=1)
+
+        extracted_features = extract_features(df, self.settings, "id", "sort", "kind", "val").sort_index()
+        extracted_features_from_random = extract_features(df_random, self.settings,
+                                                          "id", "sort", "kind", "val").sort_index()
+
+
+        six.assertCountEqual(self, extracted_features.columns, extracted_features_from_random.columns)
+
+        for col in extracted_features:
+            self.assertIsNone(np.testing.assert_array_almost_equal(extracted_features[col],
+                                                                   extracted_features_from_random[col]))
 
     def test_calculate_ts_features_after_randomisation(self):
         df = self.create_test_data_sample()
