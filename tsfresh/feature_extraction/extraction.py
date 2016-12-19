294a614802ddfce4a0e1f30538f90aed92fc6ebd
Nils Braun <nils.braun@kit.edu>
2016-12-19 18:06:33 +0100

    Refined the chunksize calculation

--- tsfresh/feature_extraction/extraction.py
+++ tsfresh/feature_extraction/extraction.py
@@ -245,13 +245,14 @@ def _calculate_best_chunksize(iterable_list, settings):
     """
     Helper function to calculate the best chunksize for a given number of elements to calculate,
     or use the one in the settings object.
-    The chunksize calculation taken from map_async of multiprocessing module.
+
+    The formula is more or less an empirical result.
     :param iterable_list: A list which defines how many calculations there need to be.
     :param settings: The settings object where the chunksize may already be given (or not).
     :return: The chunksize which should be used.
     """
     if not settings.chunksize:
-        chunksize, extra = divmod(len(iterable_list), settings.n_processes * 4)
+        chunksize, extra = divmod(len(iterable_list), settings.n_processes * 5)
         if extra:
             chunksize += 1
     else:
