f49413d6db8f0f11524059d35e5d4ae7c14c1fcd
Maximilian Christ <max.christ@me.com>
2017-01-07 11:05:37 +0100

    add warning for m>2

--- tsfresh/examples/driftbif_simulation.py
+++ tsfresh/examples/driftbif_simulation.py
@@ -6,10 +6,9 @@
 
 import pandas as pd
 import numpy as np
+import logging
 
-
-# todo: add possibility to extract data set for regression problem (estimation of tau parameter)
-# todo: add unit test
+_logger = logging.getLogger(__name__)
 
 class velocity(object):
     """
@@ -135,6 +134,10 @@ def load_driftbif(n, l, m=2, classification=True):
 
     # todo: add ratio of classes
     # todo: add start seed
+
+    if m > 2:
+        logging.warning("You set the dimension parameter for the dissipative soliton to m={}, however it is only"
+                        "properly defined for m=1 or m=2.".format(m))
 
     id = np.repeat(range(n), l * m)
     dimensions = list(np.repeat(range(m), l)) * n
