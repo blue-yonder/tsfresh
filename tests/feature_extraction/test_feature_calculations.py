4e535e4f9da9d77729bcaae93273ef32c9f7fa1f
obi@oX2015a <kempa-liehr@fmf.uni-freiburg.de>
2017-01-12 21:14:47 +1300

    Enabled test module to run stand alone.

--- tests/feature_extraction/test_feature_calculations.py
+++ tests/feature_extraction/test_feature_calculations.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division
 from __future__ import print_function
 from builtins import range
 from random import shuffle
-from unittest import TestCase
+from unittest import TestCase, main
 from tsfresh.feature_extraction.feature_calculators import *
 from tsfresh.feature_extraction.feature_calculators import _get_length_sequences_where
 import six
@@ -505,3 +505,6 @@ class FeatureCalculationTestCase(TestCase):
         self.assertEqualOnAllArrayTypes(approximate_entropy, [1, 2, 3], 0, m=2, r=0.5)
         self.assertAlmostEqualOnAllArrayTypes(approximate_entropy, [12, 13, 15, 16, 17]*10, 0.282456191, m=2, r=0.9)
         self.assertRaises(ValueError, approximate_entropy, x=[12, 13, 15, 16, 17]*10, m=2, r=-0.5)
+
+if __name__ == '__main__':
+    main()
