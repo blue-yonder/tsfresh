29332254490888666ccbab8c91eca9be7978a8a0
Maximilian Christ <max.christ@me.com>
2017-01-31 16:32:26 +0100

    accept replace dics that contain more than just columns

--- tsfresh/utilities/dataframe_functions.py
+++ tsfresh/utilities/dataframe_functions.py
@@ -119,16 +119,16 @@ def impute_dataframe_range(df_impute, col_to_max, col_to_min, col_to_median):
     columns = df_impute.columns
 
     # Making sure col_to_median, col_to_max and col_to_min have entries for every column
-    if set(col_to_median.keys()) != set(columns) or \
-                    set(col_to_max.keys()) != set(columns) or \
-                    set(col_to_min.keys()) != set(columns):
+    if not set(columns) <= set(col_to_median.keys()) or \
+            not set(columns) <= set(col_to_max.keys()) or \
+            not set(columns) <= set(col_to_min.keys()):
         raise ValueError("Some of the dictionaries col_to_median, col_to_max, col_to_min contains more or less keys "
-                   "than the column names in df")
+                         "than the column names in df")
 
     # check if there are non finite values for the replacement
     if np.any(~np.isfinite(col_to_median.values())) or \
-        np.any(~np.isfinite(col_to_min.values())) or \
-        np.any(~np.isfinite(col_to_max.values())):
+            np.any(~np.isfinite(col_to_min.values())) or \
+            np.any(~np.isfinite(col_to_max.values())):
         raise ValueError("Some of the dictionaries col_to_median, col_to_max, col_to_min contains non finite values "
                          "to to replace")
 
