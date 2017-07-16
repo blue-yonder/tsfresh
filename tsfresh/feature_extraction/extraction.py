f5bfcf629dfcc772615b56cb7d8cfdbb00443d79
Maximilian Christ <max.christ@me.com>
2017-07-16 18:01:34 +0200

    possibly turn off warnings for impute function (#245)
    solves issue #242

--- tsfresh/feature_extraction/extraction.py
+++ tsfresh/feature_extraction/extraction.py
@@ -146,11 +146,11 @@ def extract_features(timeseries_container, default_fc_parameters=None,
                                 disable_progressbar=disable_progressbar,
                                 default_fc_parameters=default_fc_parameters, kind_to_fc_parameters=kind_to_fc_parameters)
 
-    result.index = result.index.astype(df_melt[column_id].dtype)
+        result.index = result.index.astype(df_melt[column_id].dtype)
 
-    # Impute the result if requested
-    if impute_function is not None:
-        impute_function(result)
+        # Impute the result if requested
+        if impute_function is not None:
+            impute_function(result)
 
     # Turn off profiling if it was turned on
     if profile:
