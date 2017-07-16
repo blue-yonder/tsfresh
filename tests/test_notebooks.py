940b29ea3e97b710cbb48f79cb753922c3950196
Maximilian Christ <max.christ@me.com>
2017-07-16 14:03:38 +0200

    only test nbs for py2

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,9 +6,9 @@ import os
 import subprocess
 import tempfile
 import nbformat
+import six
 
 from unittest import TestCase
-
 
 def _notebook_run(path):
     """
@@ -39,29 +39,36 @@ def _notebook_run(path):
 class NotebooksTestCase(TestCase):
 
     def test_basic_pipeline_example(self):
-        nb, errors = _notebook_run('notebooks/basic_pipeline_example.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/basic_pipeline_example.ipynb')
+            self.assertEqual(errors, [])
 
     def test_human_activity_recognition_multi_class_example(self):
-        nb, errors = _notebook_run('notebooks/human_activity_recognition_multi_class_example.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/human_activity_recognition_multi_class_example.ipynb')
+            self.assertEqual(errors, [])
 
     def test_robot_failure_example(self):
-        nb, errors = _notebook_run('notebooks/robot_failure_example.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/robot_failure_example.ipynb')
+            self.assertEqual(errors, [])
 
     def test_inspect_dft_features(self):
-        nb, errors = _notebook_run('notebooks/inspect_dft_features.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/inspect_dft_features.ipynb')
+            self.assertEqual(errors, [])
 
     def test_fc_parameters_extraction_dictionary(self):
-        nb, errors = _notebook_run('notebooks/the-fc_parameters-extraction-dictionary.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/the-fc_parameters-extraction-dictionary.ipynb')
+            self.assertEqual(errors, [])
 
     def test_pipeline_with_two_datasets(self):
-        nb, errors = _notebook_run('notebooks/pipeline_with_two_datasets.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/pipeline_with_two_datasets.ipynb')
+            self.assertEqual(errors, [])
 
     def test_friedrich_coefficients(self):
-        nb, errors = _notebook_run('notebooks/friedrich_coefficients.ipynb')
-        self.assertEqual(errors, [])
+        if six.PY2:
+            nb, errors = _notebook_run('notebooks/friedrich_coefficients.ipynb')
+            self.assertEqual(errors, [])
