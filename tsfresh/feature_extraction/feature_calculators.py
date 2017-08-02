a9da584f5a72d5004bbc9d121ba9041329f345c0
Niklas H <niklas.haas@student.kit.edu>
2017-08-02 16:12:26 +0200

    Add partial autocorrelation feature calculator
    * The PACF can be used to determine the lag p of an AR(p) process
    
    * Comprehensive docstring
    
    * Many tests added. Some may be discarded
    
    * Use the option `method="ld"` which is fastest and does not throw
    Singular Matrix Errors compared to "yw"
    
    * The OLS method is buggy / wrong (see statsmodels issue #3839)
    
    * Solves tsfresh issue #223

--- tsfresh/feature_extraction/feature_calculators.py
+++ tsfresh/feature_extraction/feature_calculators.py
@@ -24,7 +24,7 @@ import pandas as pd
 from scipy.signal import welch, cwt, ricker, find_peaks_cwt
 from statsmodels.tsa.ar_model import AR
 from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import acf, pacf
 from scipy.stats import linregress
 
 # todo: make sure '_' works in parameter names in all cases, add a warning if not
@@ -289,6 +289,58 @@ def agg_autocorrelation(x, param):
     else:
         a = acf(x, unbiased=True, fft=n > 1250)[1:]
     return [("f_agg_\"{}\"".format(config["f_agg"]), getattr(np, config["f_agg"])(a)) for config in param]
+
+
+@set_property("fctype", "combiner")
+def partial_autocorrelation(x, param):
+    """
+    Calculates the value of the partial autocorrelation function at the given lag. The lag `k` partial autocorrelation
+    of a time series :math:`\\lbrace x_t, t = 1 \\ldots T \\rbrace` equals the partial correlation of :math:`x_t` and
+    :math:`x_{t-k}`, adjusted for the intermediate variables
+    :math:`\\lbrace x_{t-1}, \\ldots, x_{t-k+1} \\rbrace` ([1]).
+    Following [2], it can be defined as
+
+    .. math::
+
+        \\alpha_k = \\frac{ Cov(x_t, x_{t-k} | x_{t-1}, \\ldots, x_{t-k+1})}
+        {\\sqrt{ Var(x_t | x_{t-1}, \\ldots, x_{t-k+1}) Var(x_{t-k} | x_{t-1}, \\ldots, x_{t-k+1} )}}
+
+    with (a) :math:`x_t = f(x_{t-1}, \\ldots, x_{t-k+1})` and (b) :math:`x_{t-k} = f(x_{t-1}, \\ldots, x_{t-k+1})`
+    being AR(k-1) models that can be fitted by OLS. Be aware that in (a), the regression is done on past values to
+    predict :math:`x_t` whereas in (b), future values are used to calculate the past value :math:`x_{t-k}`.
+    It is said in [1] that "for an AR(p), the partial autocorrelations [ :math:`\\alpha_k` ] will be nonzero for `k<=p`
+    and zero for `k>p`."
+    With this property, it is used to determine the lag of an AR-Process.
+
+    .. rubric:: References
+
+    |  [1] Box, G. E., Jenkins, G. M., Reinsel, G. C., & Ljung, G. M. (2015).
+    |  Time series analysis: forecasting and control. John Wiley & Sons.
+    |  [2] https://onlinecourses.science.psu.edu/stat510/node/62
+
+    :param x: the time series to calculate the feature of
+    :type x: pandas.Series
+    :param param: contains dictionaries {"lag": val} with int val indicating the lag to be returned
+    :type param: list
+    :return: the value of this feature
+    :return type: float
+    """
+    # Check the difference between demanded lags by param and possible lags to calculate (depends on len(x))
+    max_demanded_lag = max([lag["lag"] for lag in param])
+    n = len(x)
+
+    # Check if list is too short or consists only of NaNs
+    if n <= 1 or sum(np.isnan(x)) == n:
+        pacf_coeffs = [np.nan] * (max_demanded_lag + 1)
+    else:
+        if (n <= max_demanded_lag):
+            max_lag = n - 1
+        else:
+            max_lag = max_demanded_lag
+        pacf_coeffs = list(pacf(x, method="ld", nlags=max_lag))
+        pacf_coeffs = pacf_coeffs + [np.nan] * max(0, (max_demanded_lag - max_lag))
+
+    return [("lag_{}".format(lag["lag"]), pacf_coeffs[lag["lag"]]) for lag in param]
 
 
 @set_property("fctype", "combiner")
