1a7e9ac689ce449ac8ef322795455a6cc7a44ca3
Niklas Haas <nikha@mailbox.org>
2017-08-03 12:39:32 +0200

    Add partial autocorrelation feature calculator (#283)
    * Add partial autocorrelation feature calculator
    
    * The PACF can be used to determine the lag p of an AR(p) process
    
    * Comprehensive docstring
    
    * Many tests added. Some may be discarded
    
    * Use the option `method="ld"` which is fastest and does not throw
    Singular Matrix Errors compared to "yw"
    
    * The OLS method is buggy / wrong (see statsmodels issue #3839)
    
    * Solves tsfresh issue #223
    
    * Remove check for list full of NaNs

--- tests/feature_extraction/test_feature_calculations.py
+++ tests/feature_extraction/test_feature_calculations.py
@@ -159,6 +159,70 @@ class FeatureCalculationTestCase(TestCase):
         res = dict(agg_autocorrelation(x, param=param))["f_agg_\"median\""]
         self.assertAlmostEqual(res, expected_res, places=4)
 
+    def test_partial_autocorrelation(self):
+
+        # Test for altering time series
+        # len(x) < max_lag
+        param = [{"lag": lag} for lag in range(10)]
+        x = [1, 2, 1, 2, 1, 2]
+        expected_res = [("lag_0", 1.0), ("lag_1", -1.0), ("lag_2", np.nan)]
+        res = partial_autocorrelation(x, param=param)
+        self.assertAlmostEqual(res[0][1], expected_res[0][1], places=4)
+        self.assertAlmostEqual(res[1][1], expected_res[1][1], places=4)
+        self.assertIsNaN(res[2][1])
+
+        # Linear signal
+        param = [{"lag": lag} for lag in range(10)]
+        x = np.linspace(0, 1, 3000)
+        expected_res = [("lag_0", 1.0), ("lag_1", 1.0), ("lag_2", 0)]
+        res = partial_autocorrelation(x, param=param)
+        self.assertAlmostEqual(res[0][1], expected_res[0][1], places=2)
+        self.assertAlmostEqual(res[1][1], expected_res[1][1], places=2)
+        self.assertAlmostEqual(res[2][1], expected_res[2][1], places=2)
+
+        # Random noise
+        np.random.seed(42)
+        x = np.random.normal(size=3000)
+        param = [{"lag": lag} for lag in range(10)]
+        expected_res = [("lag_0", 1.0), ("lag_1", 0), ("lag_2", 0)]
+        res = partial_autocorrelation(x, param=param)
+        self.assertAlmostEqual(res[0][1], expected_res[0][1], places=1)
+        self.assertAlmostEqual(res[1][1], expected_res[1][1], places=1)
+        self.assertAlmostEqual(res[2][1], expected_res[2][1], places=1)
+
+        # On a simulated AR process
+        np.random.seed(42)
+        param = [{"lag": lag} for lag in range(10)]
+        # Simulate AR process
+        T = 3000
+        epsilon = np.random.randn(T)
+        x = np.repeat(1.0, T)
+        for t in range(T - 1):
+            x[t + 1] = 0.5 * x[t] + 2 + epsilon[t]
+        expected_res = [("lag_0", 1.0), ("lag_1", 0.5), ("lag_2", 0)]
+        res = partial_autocorrelation(x, param=param)
+        self.assertAlmostEqual(res[0][1], expected_res[0][1], places=1)
+        self.assertAlmostEqual(res[1][1], expected_res[1][1], places=1)
+        self.assertAlmostEqual(res[2][1], expected_res[2][1], places=1)
+
+        # Some pathological cases
+        param = [{"lag": lag} for lag in range(10)]
+        # List of length 1
+        res = partial_autocorrelation([1], param=param)
+        for lag_no, lag_val in res:
+            self.assertIsNaN(lag_val)
+        # Empty list
+        res = partial_autocorrelation([], param=param)
+        for lag_no, lag_val in res:
+            self.assertIsNaN(lag_val)
+        # List contains only zeros
+        res = partial_autocorrelation(np.zeros(100), param=param)
+        for lag_no, lag_val in res:
+            if lag_no == "lag_0":
+                self.assertEqual(lag_val, 1.0)
+            else:
+                self.assertIsNaN(lag_val)
+
 
     def test_augmented_dickey_fuller(self):
         # todo: add unit test for the values of the test statistic
