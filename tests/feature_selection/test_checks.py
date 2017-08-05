3d85f143992a74d710a821b7b9d2f5fcb8841554
Julius Neuffer <julius.neuffer@gmail.com>
2017-08-05 11:29:58 +0200

    Test for nan check

--- tests/feature_selection/test_checks.py
+++ tests/feature_selection/test_checks.py
@@ -15,6 +15,22 @@ class TestFeatureSignificanceTestsChecks:
     """
     Test cases for the configuration and type tests of the feature selectors.
     """
+    @pytest.fixture()
+    def binary_series_with_nan(self):
+        return pd.Series([np.NaN, 1, 1])
+
+    @pytest.fixture()
+    def real_series_with_nan(self):
+        return pd.Series([np.NaN, 1, 2])
+
+    @pytest.fixture()
+    def binary_series(self):
+        return pd.Series([0, 1, 1])
+
+    @pytest.fixture()
+    def real_series(self):
+        return pd.Series([0, 1, 2])
+
     def test_fs_tb_fb_binary(self):
         with pytest.raises(ValueError):
             target_binary_feature_binary_test(x=pd.Series([0, 1, 2]), y=pd.Series([0, 1, 1]))
@@ -74,3 +90,37 @@ class TestFeatureSignificanceTestsChecks:
 
         with pytest.raises(TypeError):
             target_real_feature_real_test(x=pd.Series([0, 1, 2]), y=[0, 1, 2])
+
+    def test_fb_tb_feature_nan(self, binary_series_with_nan, binary_series):
+        with pytest.raises(ValueError):
+            target_binary_feature_binary_test(x=binary_series_with_nan, y=binary_series)
+
+    def test_fb_tb_target_nan(self, binary_series_with_nan, binary_series):
+        with pytest.raises(ValueError):
+            target_binary_feature_binary_test(x=binary_series, y=binary_series_with_nan)
+
+    def test_fb_tr_feature_nan(self, binary_series_with_nan, real_series):
+        with pytest.raises(ValueError):
+            target_real_feature_binary_test(x=binary_series_with_nan, y=real_series)
+
+    def test_fb_tr_target_nan(self, real_series_with_nan, binary_series):
+        with pytest.raises(ValueError):
+            target_real_feature_binary_test(x=binary_series, y=real_series_with_nan)
+
+    def test_fr_tb_feature_nan(self, real_series_with_nan, binary_series):
+        with pytest.raises(ValueError):
+            target_binary_feature_real_test(x=real_series_with_nan, y=binary_series,
+                                            test=TEST_FOR_BINARY_TARGET_REAL_FEATURE)
+
+    def test_fr_tb_target_nan(self, binary_series_with_nan, real_series):
+        with pytest.raises(ValueError):
+            target_binary_feature_real_test(x=real_series, y=binary_series_with_nan,
+                                            test=TEST_FOR_BINARY_TARGET_REAL_FEATURE)
+
+    def test_fr_tr_feature_nan(self, real_series_with_nan, real_series):
+        with pytest.raises(ValueError):
+            target_real_feature_real_test(x=real_series_with_nan, y=real_series)
+
+    def test_fr_tr_target_nan(self, real_series_with_nan, real_series):
+        with pytest.raises(ValueError):
+            target_real_feature_real_test(x=real_series, y=real_series_with_nan)
