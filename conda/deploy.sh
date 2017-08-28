43c6fc28d1442a9285770714d1bb1de4e3e5de34
Nils Braun <nils.braun@kit.edu>
2017-08-28 21:37:22 +0200

    Updated conda script

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,8 +1,17 @@
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh;
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh
+
 export PATH="$HOME/miniconda/bin:$PATH"
+export VERSION=TRAVIS_TAG 
+export BUILD_DIR= /tmp/build
+export PKG_NAME=tsfresh
+
 rehash
+
 conda install conda-build
-VERSION=TRAVIS_TAG conda build tsfresh -c conda-forge --old-build-string
 conda install anaconda-client
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u nbraun -l nightly $HOME/miniconda/conda-bld/$OS/$PKG_NAME-$TRAVIS_TAG-0.tar.bz2 --force
+
+conda build tsfresh -c conda-forge --old-build-string --output-folder $BUILD_DIR
+cd $BUILD_DIR
+conda convert $OS/PKG_NAME-$VERSION-0.tar.bz2 -p all
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u nbraun -l nightly $BUILD_DIR/*/$PKG_NAME-$VERSION-0.tar.bz2 --force
