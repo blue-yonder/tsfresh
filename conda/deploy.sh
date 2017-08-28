57fed5f5aefb3c52927cec6997e301580e5580be
Nils Braun <nils.braun@kit.edu>
2017-08-28 21:55:11 +0200

    Forget to enable batch mode

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,5 +1,9 @@
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+bash miniconda.sh  -b -p $HOME/miniconda
 
 export PATH="$HOME/miniconda/bin:$PATH"
 export VERSION=TRAVIS_TAG 
