f35bc93dae57d36dcfedbbebecedf88b493e1c1b
Maximilian Christ <max.christ@me.com>
2017-10-01 19:06:28 +0200

    add unit test for lokal dask distributor

--- tests/units/utilities/test_distribution.py
+++ tests/units/utilities/test_distribution.py
@@ -4,7 +4,11 @@
 
 from unittest import TestCase
 import numpy as np
-from tsfresh.utilities.distribution import MultiprocessingDistributor
+import pandas as pd
+
+from tsfresh import extract_features
+from tsfresh.utilities.distribution import MultiprocessingDistributor, LocalDaskDistributor
+from tests.fixtures import DataTestCase
 
 
 class MultiprocessingDistributorTestCase(TestCase):
@@ -36,6 +40,25 @@ class MultiprocessingDistributorTestCase(TestCase):
         self.assertEqual(distributor.calculate_best_chunk_size(30), 2)
         self.assertEqual(distributor.calculate_best_chunk_size(31), 3)
 
-    # todo: test the local dask cluster
-    # todo: error for test not valid distributor
-    # todo: test ipaddresses at clusterdaskdistributor
+
+class LocalDaskDistributorTestCase(DataTestCase):
+
+    def test_local_dask_cluster_extraction(self):
+
+        Distributor = LocalDaskDistributor(n_workers=1)
+
+        df = self.create_test_data_sample()
+        extracted_features = extract_features(df, column_id="id", column_sort="sort", column_kind="kind",
+                                              column_value="val",
+                                              distributor=Distributor)
+
+        self.assertIsInstance(extracted_features, pd.DataFrame)
+        self.assertTrue(np.all(extracted_features.a__maximum == np.array([71, 77])))
+        self.assertTrue(np.all(extracted_features.a__sum_values == np.array([691, 1017])))
+        self.assertTrue(np.all(extracted_features.a__abs_energy == np.array([32211, 63167])))
+        self.assertTrue(np.all(extracted_features.b__sum_values == np.array([757, 695])))
+        self.assertTrue(np.all(extracted_features.b__minimum == np.array([3, 1])))
+        self.assertTrue(np.all(extracted_features.b__abs_energy == np.array([36619, 35483])))
+        self.assertTrue(np.all(extracted_features.b__mean == np.array([37.85, 34.75])))
+        self.assertTrue(np.all(extracted_features.b__median == np.array([39.5, 28.0])))
+
