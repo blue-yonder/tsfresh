b3101eddfb1df295fe6c475fe31c0b0751f9e01d
earthgecko <gary.wilson@of-networks.co.uk>
2018-08-20 15:54:39 +0100

    increase-timeouts-notebook-tests #409
    - Set default_timeout = 900 and passing through to each notebook test
    - Also includes a commented out section to disable notebook tests on Travis if
      this is desired at any point in the future.
    
    Modified:
    tests/integrations/test_notebooks.py

--- tests/integrations/test_notebooks.py
+++ tests/integrations/test_notebooks.py
@@ -10,7 +10,10 @@ import six
 
 from unittest import TestCase
 
-def _notebook_run(path):
+default_timeout = 900
+
+
+def _notebook_run(path, timeout=default_timeout):
     """
     Execute a singular ipython notebook at path and returns the cells and outputs
 
@@ -18,11 +21,15 @@ def _notebook_run(path):
     """
 
     dirname, _ = os.path.split(path)
+    execproc_timeout = '--ExecutePreprocessor.timeout=%d' % timeout
+
+    # Do not run notebook tests on Travis
+    # if os.environ['TRAVIS']:
+    #     return [], []
 
     with tempfile.NamedTemporaryFile(mode='w+t', suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert",
-                "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=600"]
+                "--to", "notebook", "--execute", execproc_timeout]
         if six.PY2:
             args += ["--ExecutePreprocessor.kernel_name=python2"]
         elif six.PY3:
@@ -42,29 +49,29 @@ def _notebook_run(path):
 class NotebooksTestCase(TestCase):
 
     def test_basic_pipeline_example(self):
-        nb, errors = _notebook_run('notebooks/basic_pipeline_example.ipynb')
+        nb, errors = _notebook_run('notebooks/basic_pipeline_example.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_human_activity_recognition_multi_class_example(self):
-        nb, errors = _notebook_run('notebooks/human_activity_recognition_multi_class_example.ipynb')
+        nb, errors = _notebook_run('notebooks/human_activity_recognition_multi_class_example.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_robot_failure_example(self):
-        nb, errors = _notebook_run('notebooks/robot_failure_example.ipynb')
+        nb, errors = _notebook_run('notebooks/robot_failure_example.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_inspect_dft_features(self):
-        nb, errors = _notebook_run('notebooks/inspect_dft_features.ipynb')
+        nb, errors = _notebook_run('notebooks/inspect_dft_features.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_fc_parameters_extraction_dictionary(self):
-        nb, errors = _notebook_run('notebooks/the-fc_parameters-extraction-dictionary.ipynb')
+        nb, errors = _notebook_run('notebooks/the-fc_parameters-extraction-dictionary.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_pipeline_with_two_datasets(self):
-        nb, errors = _notebook_run('notebooks/pipeline_with_two_datasets.ipynb')
+        nb, errors = _notebook_run('notebooks/pipeline_with_two_datasets.ipynb', default_timeout)
         self.assertEqual(errors, [])
 
     def test_friedrich_coefficients(self):
-        nb, errors = _notebook_run('notebooks/friedrich_coefficients.ipynb')
+        nb, errors = _notebook_run('notebooks/friedrich_coefficients.ipynb', default_timeout)
         self.assertEqual(errors, [])
