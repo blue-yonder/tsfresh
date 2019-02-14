732b7d78ffea04000775f0149a5560afe03bd53d
Roman Yurchak <rth.yurchak@pm.me>
2019-02-14 10:57:19 +0100

    MAINT Remove pyscaffold build dependency (#487)
    * Improve setup.py
    * Remove setup.py test

--- setup.py
+++ setup.py
@@ -9,15 +9,22 @@
 """
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
 
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx', 'sphinx_rtd_theme'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
+with open("requirements.txt") as f:
+    requirements = [line for line in f if not line.startswith("#")]
 
+needs_sphinx = {"build_sphinx", "upload_docs"}.intersection(sys.argv)
+sphinx = ["sphinx", "sphinx_rtd_theme"] if needs_sphinx else []
 
-if __name__ == "__main__":
-    setup_package()
+setup(
+    use_scm_version=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    setup_requires=["six", "setuptools_scm"] + sphinx,
+    packages=find_packages(),
+    install_requires=requirements,
+)
