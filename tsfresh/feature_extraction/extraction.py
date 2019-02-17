df79a6f0d90301339cbc82f9297a84f8262971f8
Maximilian Christ <max.christ@me.com>
2019-02-17 23:52:46 +0100

    prepare changelog for 0.11.2

--- tsfresh/feature_extraction/extraction.py
+++ tsfresh/feature_extraction/extraction.py
@@ -70,7 +70,8 @@ def extract_features(timeseries_container, default_fc_parameters=None,
 
     :param kind_to_fc_parameters: mapping from kind names to objects of the same type as the ones for
             default_fc_parameters. If you put a kind as a key here, the fc_parameters
-            object (which is the value), will be used instead of the default_fc_parameters.
+            object (which is the value), will be used instead of the default_fc_parameters. This means that kinds, for
+            which kind_of_fc_parameters doe not have any entries, will be ignored by the feature selection.
     :type kind_to_fc_parameters: dict
 
     :param column_id: The name of the id column to group by.
