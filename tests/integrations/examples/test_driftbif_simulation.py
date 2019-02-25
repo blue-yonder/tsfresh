5e887bb0070c19068b2aadef7a0531bfd1baf4e6
Maximilian Christ <max.christ@me.com>
2019-02-25 08:56:49 +0100

    Issue413 sampletau (#500)
    * refactoring drifbif_simulation.py
    * remove test_tsfresh_baseline_dataset
    * update threshold in test_ratio to 0.0006, refactoring

--- tests/integrations/examples/test_driftbif_simulation.py
+++ tests/integrations/examples/test_driftbif_simulation.py
@@ -14,45 +14,44 @@ class DriftBifSimlationTestCase(unittest.TestCase):
         """
         The intrinsic velocity of a dissipative soliton at the Drift bifurcation point is zero.
         """
-        ds = velocity(tau=1.0/0.3)
+        ds = velocity(tau=1.0 / 0.3)
         self.assertEqual(ds.deterministic, 0.0)
 
     def test_relaxation_dynamics(self):
         """
         Test accuracy of integrating the deterministic dynamics [6, p. 116]
         """
-        ds = velocity(tau=1.01/0.3, R=0)
+        ds = velocity(tau=1.01 / 0.3, R=0)
         v0 = 1.01 * ds.deterministic
 
-        Nt = 100 # Number of time steps
+        Nt = 100  # Number of time steps
         v = ds.simulate(Nt, v0=np.array([v0, 0.]))
 
         k3t = ds.kappa_3 * ds.tau
-        k3st = ds.kappa_3**2 * ds.tau
+        k3st = ds.kappa_3 ** 2 * ds.tau
         a0 = v0 / ds.kappa_3
         acceleration = lambda t: ds.kappa_3 * (a0 * np.sqrt(k3t - 1) * np.exp(k3st * t) /
-                                               np.sqrt(np.exp(2.0 * k3st * t) * ds.Q * a0**2 +
-                                                       np.exp(2.0 * ds.kappa_3 * t) * (k3t - 1 - ds.Q * a0**2)))
+                                               np.sqrt(np.exp(2.0 * k3st * t) * ds.Q * a0 ** 2 +
+                                                       np.exp(2.0 * ds.kappa_3 * t) * (k3t - 1 - ds.Q * a0 ** 2)))
         t = ds.delta_t * np.arange(Nt)
-        return np.testing.assert_array_almost_equal(v[:,0], np.vectorize(acceleration)(t),
+        return np.testing.assert_array_almost_equal(v[:, 0], np.vectorize(acceleration)(t),
                                                     decimal=8)
 
     def test_equlibrium_velocity(self):
         """
         Test accuracy of integrating the deterministic dynamics for equilibrium velocity [6, p. 116]
         """
-        ds = velocity(tau=1.01/0.3, R=0)
+        ds = velocity(tau=1.01 / 0.3, R=0)
         v0 = ds.deterministic
 
-        Nt = 100 # Number of time steps
+        Nt = 100  # Number of time steps
         v = ds.simulate(Nt, v0=np.array([v0, 0.]))
 
-        return np.testing.assert_array_almost_equal(v[:,0]-v0, np.zeros(Nt),
+        return np.testing.assert_array_almost_equal(v[:, 0] - v0, np.zeros(Nt),
                                                     decimal=8)
 
-
     def test_dimensionality(self):
-        ds = velocity(tau=1.0/0.3)
+        ds = velocity(tau=1.0 / 0.3)
         Nt = 10
         v = ds.simulate(Nt)
         self.assertEqual(v.shape, (Nt, 2),
@@ -61,6 +60,7 @@ class DriftBifSimlationTestCase(unittest.TestCase):
         v = ds.simulate(Nt, v0=np.zeros(3))
         self.assertEqual(v.shape, (Nt, 3),
                          'The returned vector should reflect the dimension of the initial condition.')
+
 
 class SampleTauTestCase(unittest.TestCase):
     def test_range(self):
@@ -71,14 +71,15 @@ class SampleTauTestCase(unittest.TestCase):
     def test_ratio(self):
         tau = sample_tau(100000, ratio=0.4)
         sample = np.array(tau)
-        before = np.sum(sample <= 1/0.3)
-        beyond = np.sum(sample > 1/0.3)
-        self.assertTrue(abs(0.4 - float(before)/(before+beyond))<0.002)
-        
+        before = np.sum(sample <= 1 / 0.3)
+        beyond = np.sum(sample > 1 / 0.3)
+        self.assertTrue(abs(0.4 - float(before) / (before + beyond)) < 0.006)
+
+
 class LoadDriftBifTestCase(unittest.TestCase):
     def test_classification_labels(self):
         X, y = load_driftbif(10, 100)
-        self.assertEqual(set(y), set([0,1]))
+        self.assertEqual(set(y), set([0, 1]))
 
     def test_regression_labels(self):
         Nsamples = 10
@@ -97,6 +98,3 @@ class LoadDriftBifTestCase(unittest.TestCase):
         Nt = 100
         X, y = load_driftbif(Nsamples, Nt, m=3)
         self.assertEqual(X.shape, (3 * Nt * Nsamples, 4))
-
-if __name__ == '__main__':
-    unittest.main()
