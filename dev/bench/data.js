70338a6e848d2c4045773edcb68d947c3342424d
github-action-benchmark <github@users.noreply.github.com>
2020-06-25 09:54:35 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for c40a057d41458303135fdde13c0cd0b8933400c9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1591648921937,
+  "lastUpdate": 1593078875567,
   "repoUrl": "https://github.com/blue-yonder/tsfresh",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -86,6 +86,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0009091646652443717",
             "extra": "mean: 4.766455180499989 sec\nrounds: 2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "blue-yonder",
+            "username": "blue-yonder"
+          },
+          "committer": {
+            "name": "blue-yonder",
+            "username": "blue-yonder"
+          },
+          "id": "c40a057d41458303135fdde13c0cd0b8933400c9",
+          "message": "Add benchmarking test",
+          "timestamp": "2020-06-25T08:06:27Z",
+          "url": "https://github.com/blue-yonder/tsfresh/pull/710/commits/c40a057d41458303135fdde13c0cd0b8933400c9"
+        },
+        "date": 1593078875111,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmark.py::test_benchmark_small_data",
+            "value": 0.20656169377173203,
+            "unit": "iter/sec",
+            "range": "stddev: 0.009986355226502463",
+            "extra": "mean: 4.841168668499996 sec\nrounds: 2"
+          },
+          {
+            "name": "tests/benchmark.py::test_benchmark_large_data",
+            "value": 0.3767772633671491,
+            "unit": "iter/sec",
+            "range": "stddev: 0.029941877327895133",
+            "extra": "mean: 2.6540879645000075 sec\nrounds: 2"
+          },
+          {
+            "name": "tests/benchmark.py::test_benchmark_with_selection",
+            "value": 0.2265800912702651,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0011348349660196216",
+            "extra": "mean: 4.413450424499999 sec\nrounds: 2"
           }
         ]
       }
