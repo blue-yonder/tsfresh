36fe92935e579b45820a9ec88e45062b13d02762
Emanuele Fumagalli <efumagal@synamedia.com>
2020-06-28 10:32:55 +0100

    working on reoccurring_values comments

--- tsfresh/feature_extraction/feature_calculators.py
+++ tsfresh/feature_extraction/feature_calculators.py
@@ -865,7 +865,7 @@ def first_location_of_minimum(x):
 @set_property("fctype", "simple")
 def percentage_of_reoccurring_values_to_all_values(x):
     """
-    Returns the percentage of unique values, that are present in the time series
+    Returns the percentage of values that are present in the time series
     more than once.
 
         len(different values occurring more than once) / len(different values)
@@ -893,8 +893,8 @@ def percentage_of_reoccurring_values_to_all_values(x):
 @set_property("input", "pd.Series")
 def percentage_of_reoccurring_datapoints_to_all_datapoints(x):
     """
-    Returns the ratio of unique values, that are present in the time series
-    more than once.
+    Returns the percentage of non-unique data points. Non-unique means that they are
+    contained another time in the time series again.
 
         # of data points occurring more than once / # of all data points
 
