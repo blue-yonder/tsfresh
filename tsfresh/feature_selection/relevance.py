b3f6d689d540398de99fc2f92ef808b9e08790d5
Joris Clement <j.clement@campus.tu-berlin.de>
2020-12-21 21:06:05 +0100

    Improve description for relevance table, #762
    Describe different, additional columns in relevance table for
    multiclass case.

--- tsfresh/feature_selection/relevance.py
+++ tsfresh/feature_selection/relevance.py
@@ -150,7 +150,12 @@ def calculate_relevance_table(
              "type" (binary, real or const),
              "p_value" (the significance of this feature as a p-value, lower means more significant)
              "relevant" (True if the Benjamini Hochberg procedure rejected the null hypothesis [the feature is
-             not relevant] for this feature)
+             not relevant] for this feature).
+             If problem is `multiclass` with n classes, the DataFrame will contain n
+             columns named "p_value_CLASSID" instead of the "p_value" column.
+             `CLASSID` refers here to the different values set in `y`.
+             There will also be n columns named `relevant_CLASSID`, indicating whether
+             the feature is relevant for that class.
     :rtype: pandas.DataFrame
     """
 
