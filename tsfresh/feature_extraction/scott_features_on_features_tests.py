6f7ecd9cd131b1078ef4122ff0bfd03fa028d863
Scott_Simmons23 <ssim323@aucklanduni.ac.nz>
2022-02-23 18:16:16 +1300

    first

---
+++ tsfresh/feature_extraction/scott_features_on_features_tests.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from extraction import extract_features_on_sub_features
+from tsfresh.feature_extraction.settings import MinimalFCParameters
+# Test is going to go here. Should work with Louis code + Scott code + the code that is integrated into tsfresh
+
+# Read in data
+ts = pd.read_csv("test_data.csv")
+
+
+X = extract_features_on_sub_features(timeseries_container = ts,
+                                     sub_feature_split = 2,
+                                     sub_default_fc_parameters = MinimalFCParameters,
+                                     default_fc_parameters = MinimalFCParameters,
+                                     column_id = "measurement_id",
+                                     column_sort = "t",
+                                     column_kind = None,
+                                     column_value = None)
