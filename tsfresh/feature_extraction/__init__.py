086de46a129acbf914409b2ef761f04595cfb546
Louis Jarvis <ljar069@aucklanduni.ac.nz>
2022-06-25 13:42:42 +1200

    rename + tidy notebook content, still WIP

--- tsfresh/feature_extraction/__init__.py
+++ tsfresh/feature_extraction/__init__.py
@@ -2,6 +2,6 @@
 The :mod:`tsfresh.feature_extraction` module contains methods to extract the features from the time series
 """
 
-from tsfresh.feature_extraction.extraction import extract_features
+from tsfresh.feature_extraction.extraction import extract_features, extract_features_on_sub_features
 from tsfresh.feature_extraction.settings import ComprehensiveFCParameters, MinimalFCParameters, \
     EfficientFCParameters
