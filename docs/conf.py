35175ed37e1fd8a1b45b3759c7fd96c36037a9c5
Nils Braun <nilslennartbraun@gmail.com>
2023-02-21 22:51:50 +0100

    Update to newest shinx version

--- docs/conf.py
+++ docs/conf.py
@@ -41,20 +41,14 @@ if on_rtd:
 
 # -- General configuration -----------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "1.6.4"
-
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
     "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
-    "sphinx.ext.coverage",
     "sphinx.ext.doctest",
-    "sphinx.ext.ifconfig",
     "sphinx.ext.imgmath",
     "sphinx.ext.napoleon",
 ]
@@ -74,7 +68,7 @@ master_doc = "index"
 # General information about the project.
 now = datetime.datetime.today()
 project = "tsfresh"
-copyright = "2016-{}, Maximilian Christ et al./ Blue Yonder GmbH".format(now.year)
+copyright = "2023-{}, Maximilian Christ et al./ Blue Yonder GmbH".format(now.year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -99,33 +93,9 @@ release = ""  # Is set by calling `setup.py docs`
 # directories to ignore when looking for source files.
 exclude_patterns = ["_build", "api/tests*"]
 
-# The reST default role (used for this markup: `text`) to use for all documents.
-# default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-# add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-# add_module_names = False
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-# show_authors = False
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-# A list of ignored prefixes for module index sorting.
-# modindex_common_prefix = ["tsfresh", "tsfresh.convenience"]
-
-# If true, keep warnings as "system message" paragraphs in the built documents.
-keep_warnings = True
-
 # Boolean indicating whether to scan all found documents for autosummary
 # directives, and to generate stub pages for each
 autosummary_generate = True
-
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -138,9 +108,6 @@ html_theme = "sphinx_rtd_theme"
 # documentation.
 html_theme_options = {"style_nav_header_background": "#51b63c"}
 
-# Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
-
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 try:
@@ -150,120 +117,15 @@ except ImportError:
 else:
     release = version
 
-# A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = ""
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = None
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
 # From https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
-html_context = {
-    "css_files": [
-        "_static/theme_override.css",  # override wide tables in RTD theme
-    ],
-}
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-# html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-# html_additional_pages = {}
-
-# If false, no module index is generated.
-# html_domain_indices = True
-
-# If false, no index is generated.
-# html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-# html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-# html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-# html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-# html_file_suffix = None
+html_css_files = [
+    'theme_override.css',
+]
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "tsfresh-doc"
-
-
-# -- Options for LaTeX output --------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    # 'preamble': '',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-    ("index", "user_guide.tex", "tsfresh Documentation", "", "manual"),
-]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-# latex_logo = ""
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-# latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-# latex_appendices = []
-
-# If false, no module index is generated.
-# latex_domain_indices = True
-
-# -- External mapping ------------------------------------------------------------
-python_version = ".".join(map(str, sys.version_info[0:2]))
-intersphinx_mapping = {
-    "sphinx": ("http://sphinx.pocoo.org", None),
-    "python": ("http://docs.python.org/" + python_version, None),
-    "matplotlib": ("http://matplotlib.sourceforge.net", None),
-    "numpy": ("http://docs.scipy.org/doc/numpy", None),
-    "sklearn": ("http://scikit-learn.org/stable", None),
-    "pandas": ("http://pandas.pydata.org/pandas-docs/stable", None),
-    "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
-}
