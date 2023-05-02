287a6b1ec8e42598c4a787df4918d966dce72655
Nils Braun <nilslennartbraun@gmail.com>
2023-05-02 22:57:23 +0200

    Use a more modern package layout with a full setup.cfg

--- setup.py
+++ setup.py
@@ -1,32 +1,23 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
     Setup file for tsfresh.
+    Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 2.5.6, a tool that easily
-    puts up a scaffold for your new Python project. Learn more under:
-    http://pyscaffold.readthedocs.org/
+    This file was generated with PyScaffold 3.2.3.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
 """
-
 import sys
 
-from setuptools import find_packages, setup
+from pkg_resources import VersionConflict, require
+from setuptools import setup
 
-with open("README.md") as f:
-    long_description = f.read()
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
-with open("requirements.txt") as f:
-    requirements = [line for line in f if not line.startswith("#")]
 
-needs_sphinx = {"build_sphinx", "upload_docs"}.intersection(sys.argv)
-sphinx = ["sphinx", "sphinx_rtd_theme"] if needs_sphinx else []
-
-setup(
-    use_scm_version=True,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    setup_requires=["setuptools_scm"] + sphinx,
-    packages=find_packages(exclude=["tests.*", "tests"]),
-    install_requires=requirements,
-    extras_require={"matrixprofile": ["matrixprofile>=1.1.10,<2.0.0"]},
-)
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
