1ab70aa199334c525bba1fadb5812bbbdd63c023
涛 <18964025292g@gmail.com>
2023-10-06 04:15:49 -0400

    Update feature_calculators.py

--- tsfresh/feature_extraction/feature_calculators.py
+++ tsfresh/feature_extraction/feature_calculators.py
@@ -757,8 +757,7 @@ def skewness(x):
     """
     if not isinstance(x, pd.Series):
         x = pd.Series(x)
-    return pd.Series.skew(x)
-
+    return pd.Series.skew(x, skipna = False)
 
 @set_property("fctype", "simple")
 @set_property("input", "pd.Series")
